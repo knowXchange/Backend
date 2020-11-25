abf11ba6c68622945ed8de9b3f578d5a11a4ab0b
Javier Ortiz <jortizsi@unal.edu.co>
2020-11-25 17:10:52 -0500

    Controller de RESENA

--- Backend-Application/src/main/java/KnowXchange/Backend/Application/Controller/ReviewController.java
+++ Backend-Application/src/main/java/KnowXchange/Backend/Application/Controller/ReviewController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import KnowXchange.Backend.Application.Model.Answer;
 import KnowXchange.Backend.Application.Model.Course;
 import KnowXchange.Backend.Application.Model.Lesson;
 import KnowXchange.Backend.Application.Model.Review;
@@ -86,6 +87,94 @@ public class ReviewController {
 		
 		//________________________________________________________________________________________________________
 	
+		
+		
+
+		//________________________________________________________________________________________________________
+
+		//OBTENER RESENA POR ID DEL OBJETO
+
+
+		@GetMapping(path="/getByIdByPathVariable/{id}")
+		  public @ResponseBody Review getReviewByIdByPathVariable(@PathVariable(value = "id")Integer id) {
+			  return reviewRepository.findById(id).orElseThrow(() -> new RuntimeException());
+		}
+
+		@GetMapping(path="/getByIdByRequestParameter")
+		  public @ResponseBody Review getReviewByIdByRequestParameter(
+				  @RequestParam Integer id) {
+			return reviewRepository.findById(id).orElseThrow(() -> new RuntimeException());
+		}
+
+
+		//____________________________________________________________________________________
+		
+		
+		
+		
+		
+
+		//____________________________________________________________________________________
+
+			//BORRAR-ELIMINAR UNA RESENA
+			  //se recibe el id 
+			
+			
+			
+			@DeleteMapping(path = "/deleteByPathVariable/{id}")
+			public @ResponseBody String deleteQuestionByPathVariable(@PathVariable(value = "id")Integer id) {
+				  Review reviewReceived = reviewRepository.findById(id).orElseThrow(() -> new RuntimeException());
+				  reviewRepository.delete(reviewReceived);
+				  return "Deleted";
+			}
+			
+			
+			@DeleteMapping(path = "/deleteByRequestParameter")
+			public @ResponseBody String deleteByRequestParameter(@RequestParam Integer id) {
+				  Review reviewReceived = reviewRepository.findById(id).orElseThrow(() -> new RuntimeException());
+				  reviewRepository.delete(reviewReceived);
+				  return "Deleted";
+			}
+			//__________________________________________________________________________________
+			
+
+			
+			
+
+			//__________________________________________________________________________________
+
+				//OBTENER TODASLAS RESENAS DE UN CURSO. 
+				   //recibe el id del curso de interes
+				
+				
+				
+				@GetMapping(path = "/getReviewsByPathVariable/{id}")
+				public @ResponseBody ArrayList<Review> getQuestionAnswersByPathVariable(@PathVariable(value = "id")Integer id) {
+					  ArrayList<Review> reviews = new ArrayList<>();
+					
+						for(Review c : reviewRepository.findAll()) {
+							if(c.getReviewed_Course().getId() == id) {
+								c.setReviewed_Course(null);
+								reviews.add(c);				
+							}
+						}	
+						return reviews;
+				}
+				
+				
+				@GetMapping(path = "/getReviewsByRequestParameter")
+				public @ResponseBody ArrayList<Review> getQuestioAnswersByRequestParameter(@RequestParam Integer id) {
+					ArrayList<Review> reviews = new ArrayList<>();
+					
+					for(Review c : reviewRepository.findAll()) {
+						if(c.getReviewed_Course().getId() == id) {
+							c.setReviewed_Course(null);
+							reviews.add(c);				
+						}
+					}	
+					return reviews;
+				}
+			//___________________________________________________________________________________
 	
 
 }
